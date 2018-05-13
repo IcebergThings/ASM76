5ea3b98de09b82c00b4ac8357c2a94cd00a64e39
Cheng Cao <bobcaocheng@163.com>
2018-05-13 07:20:21 -0500

    putb puti putl
    Signed-off-by: Cheng Cao <bobcaocheng@163.com>

--- ASM76/VMexec.cxx
+++ ASM76/VMexec.cxx
@@ -23,11 +23,29 @@ static uint32_t bios_call_puts(uint8_t* input) {
 	return 0x0;
 }
 
+static uint32_t bios_call_putb(uint8_t* input) {
+	printf("%d\n", *((char*) input));
+	return 0x0;
+}
+
+static uint32_t bios_call_puti(uint8_t* input) {
+	printf("%d\n", *((int*) input));
+	return 0x0;
+}
+
+static uint32_t bios_call_putl(uint8_t* input) {
+	printf("%ld\n", *((long*) input));
+	return 0x0;
+}
+
 BIOS* load_bios() {
 	BIOS b(5);
 	b.function_table[1] = &test_bios_call;
 	b.function_table[2] = &bios_call_putc;
 	b.function_table[3] = &bios_call_puts;
+	b.function_table[4] = &bios_call_putb;
+	b.function_table[5] = &bios_call_puti;
+	b.function_table[6] = &bios_call_putl;
 
 	return &b;
 }
